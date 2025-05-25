79f846e939630e34876d8fc9f5d403f7900d6d6e
Kudae Sithu <kudaesithu2@gmail.com>
2025-05-25 22:12:27 +0630

    clerk-setup

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,5 +1,13 @@
 import type { Metadata } from "next";
 import "../globals.css";
+import {
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  UserButton,
+  ClerkProvider,
+} from "@clerk/nextjs";
 
 export const metadata: Metadata = {
   title: "Authentication",
@@ -12,18 +20,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className="min-h-screen bg-gradient-to-br from-blue-100 via-purple-100 to-pink-100 flex items-center justify-center">
-        <div className="w-full max-w-md mx-auto bg-white/90 rounded-xl shadow-lg p-8 flex flex-col gap-6">
-          <header className="text-center mb-4">
-            <h1 className="text-3xl font-bold text-gray-800 mb-2">Welcome</h1>
-            <p className="text-gray-500 text-sm">
-              Please login or register to continue
-            </p>
-          </header>
-          {children}
-        </div>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className="min-h-screen bg-gradient-to-br from-blue-100 via-purple-100 to-pink-100 flex items-center justify-center">
+          <div className="w-full max-w-md mx-auto bg-white/90 rounded-xl shadow-lg p-8 flex flex-col gap-6">
+            <header className="text-center mb-4">
+              <h1 className="text-3xl font-bold text-gray-800 mb-2">Welcome</h1>
+              <p className="text-gray-500 text-sm">
+                Please login or register to continue
+              </p>
+            </header>
+            {children}
+          </div>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "../globals.css";
 import Header from "./components/header";
 import Footer from "./components/footer";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,12 +15,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>
-        <Header />
-        {children}
-        <Footer />
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          <Header />
+          {children}
+          <Footer />
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
