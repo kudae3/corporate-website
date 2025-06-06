a251dd4ecbe12643ba0ba56f086789a3e388644b
Kudae Sithu <kudaesithu2@gmail.com>
2025-06-06 14:37:15 +0630

    feat: enhance user fetching to merge data from MongoDB and Clerk API

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -3,13 +3,30 @@ import errorResponse from "@/lib/responses/errorResponse";
 import successResponse from "@/lib/responses/successResponse";
 import User from "@/models/user.model";
 import { userSchema } from "@/schemas/UserSchema";
+import { clerkClient } from "@clerk/nextjs/server";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET() {
   try {
     await dbConnect();
-    const users = await User.find();
-    return successResponse("Users fetched successfully", users, 200);
+    // users response by mongodB
+    const mongoUsers = await User.find();
+
+    // users response by clerks
+    const client = await clerkClient();
+    const clerkUsers = (await client.users.getUserList()).data;
+
+    const mergedUsers = mongoUsers.map((user) => {
+      const clerkProfile = clerkUsers.find(
+        (clerk) => clerk.id === user.clerkId
+      );
+      return {
+        ...user.toObject(),
+        clerkProfile,
+      };
+    });
+
+    return successResponse("Users fetched successfully", mergedUsers, 200);
   } catch (error) {
     return errorResponse("Failed to fetch users", error, 500);
   }
