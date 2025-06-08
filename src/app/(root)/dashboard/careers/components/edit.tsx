8e3966b0853c5cdbe55a682d91941961c3af73d5
Kudae Sithu <kudaesithu2@gmail.com>
2025-06-08 16:40:26 +0630

    feat: add tailwind-scrollbar dependency and integrate scrollbar styles in global CSS

--- src/app/(root)/dashboard/careers/components/edit.tsx
+++ src/app/(root)/dashboard/careers/components/edit.tsx
@@ -48,135 +48,137 @@ const Edit = ({ career }: { career: CareerType }) => {
         </button>
       }
     >
-      <form action="" method="post" className="space-y-5 overflow-y-auto">
-        {/* title */}
-        <div>
-          <label
-            htmlFor="title"
-            className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2"
-          >
-            Title *
-          </label>
-          <input
-            type="text"
-            id="title"
-            name="title"
-            required
-            onChange={(e) =>
-              setFormData({ ...formData, title: e.target.value })
-            }
-            value={formData.title}
-            placeholder="Enter the title"
-            className="w-full px-4 py-3 rounded-lg focus:outline-hidden dark:bg-gray-700 dark:text-white placeholder-gray-400 transition-colors"
-          />
-        </div>
-        {/* location */}
-        <div>
-          <label
-            htmlFor="location"
-            className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2"
-          >
-            Location *
-          </label>
-          <input
-            type="location"
-            id="location"
-            name="location"
-            required
-            onChange={(e) =>
-              setFormData({ ...formData, location: e.target.value })
-            }
-            value={formData.location}
-            placeholder="Enter the location"
-            className="w-full px-4 py-3 rounded-lg focus:outline-hidden dark:bg-gray-700 dark:text-white placeholder-gray-400 transition-colors"
-          />
-        </div>
-        {/* type */}
-        <div>
-          <label
-            htmlFor="type"
-            className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2"
-          >
-            Type *
-          </label>
-          <select
-            id="type"
-            name="type"
-            required
-            onChange={(e) =>
-              setFormData({
-                ...formData,
-                type: e.target.value as CareerType["type"],
-              })
-            }
-            value={formData.type}
-            className="w-full px-4 py-3 rounded-lg focus:outline-hidden dark:bg-gray-700 dark:text-white placeholder-gray-400 transition-colors"
-          >
-            <option value="full-time">Full-Time</option>
-            <option value="part-time">Part-Time</option>
-            <option value="internship">Internship</option>
-          </select>
-        </div>
-        {/* description */}
-        <div>
-          <label
-            htmlFor="description"
-            className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2"
-          >
-            description *
-          </label>
-          <input
-            type="description"
-            id="description"
-            name="description"
-            required
-            onChange={(e) =>
-              setFormData({ ...formData, description: e.target.value })
-            }
-            value={formData.description}
-            placeholder="Enter the description"
-            className="w-full px-4 py-3 rounded-lg focus:outline-hidden dark:bg-gray-700 dark:text-white placeholder-gray-400 transition-colors"
-          />
-        </div>
-        {/* salary */}
-        <div>
-          <label
-            htmlFor="salary"
-            className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2"
-          >
-            salary *
-          </label>
-          <input
-            type="salary"
-            id="salary"
-            name="salary"
-            required
-            onChange={(e) =>
-              setFormData({ ...formData, salary: e.target.value })
-            }
-            value={formData.salary}
-            placeholder="Enter the description"
-            className="w-full px-4 py-3 rounded-lg focus:outline-hidden dark:bg-gray-700 dark:text-white placeholder-gray-400 transition-colors"
-          />
-        </div>
-        {/* requirements */}
-        <div>
-          <label
-            htmlFor="requirements"
-            className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2"
-          >
-            requirements *
-          </label>
+      <div className="max-h-[600px] overflow-y-auto scrollbar-thin scrollbar-thumb-gray-200 scrollbar-track-transparent hover:scrollbar-thumb-gray-300 dark:scrollbar-thumb-gray-700 dark:hover:scrollbar-thumb-gray-600">
+        <form action="" method="post" className="space-y-5 overflow-y-auto">
+          {/* title */}
+          <div>
+            <label
+              htmlFor="title"
+              className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2"
+            >
+              Title *
+            </label>
+            <input
+              type="text"
+              id="title"
+              name="title"
+              required
+              onChange={(e) =>
+                setFormData({ ...formData, title: e.target.value })
+              }
+              value={formData.title}
+              placeholder="Enter the title"
+              className="w-full px-4 py-3 rounded-lg focus:outline-hidden dark:bg-gray-700 dark:text-white placeholder-gray-400 transition-colors"
+            />
+          </div>
+          {/* location */}
+          <div>
+            <label
+              htmlFor="location"
+              className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2"
+            >
+              Location *
+            </label>
+            <input
+              type="location"
+              id="location"
+              name="location"
+              required
+              onChange={(e) =>
+                setFormData({ ...formData, location: e.target.value })
+              }
+              value={formData.location}
+              placeholder="Enter the location"
+              className="w-full px-4 py-3 rounded-lg focus:outline-hidden dark:bg-gray-700 dark:text-white placeholder-gray-400 transition-colors"
+            />
+          </div>
+          {/* type */}
+          <div>
+            <label
+              htmlFor="type"
+              className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2"
+            >
+              Type *
+            </label>
+            <select
+              id="type"
+              name="type"
+              required
+              onChange={(e) =>
+                setFormData({
+                  ...formData,
+                  type: e.target.value as CareerType["type"],
+                })
+              }
+              value={formData.type}
+              className="w-full px-4 py-3 rounded-lg focus:outline-hidden dark:bg-gray-700 dark:text-white placeholder-gray-400 transition-colors"
+            >
+              <option value="full-time">Full-Time</option>
+              <option value="part-time">Part-Time</option>
+              <option value="internship">Internship</option>
+            </select>
+          </div>
+          {/* description */}
+          <div>
+            <label
+              htmlFor="description"
+              className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2"
+            >
+              description *
+            </label>
+            <input
+              type="description"
+              id="description"
+              name="description"
+              required
+              onChange={(e) =>
+                setFormData({ ...formData, description: e.target.value })
+              }
+              value={formData.description}
+              placeholder="Enter the description"
+              className="w-full px-4 py-3 rounded-lg focus:outline-hidden dark:bg-gray-700 dark:text-white placeholder-gray-400 transition-colors"
+            />
+          </div>
+          {/* salary */}
+          <div>
+            <label
+              htmlFor="salary"
+              className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2"
+            >
+              salary *
+            </label>
+            <input
+              type="salary"
+              id="salary"
+              name="salary"
+              required
+              onChange={(e) =>
+                setFormData({ ...formData, salary: e.target.value })
+              }
+              value={formData.salary}
+              placeholder="Enter the description"
+              className="w-full px-4 py-3 rounded-lg focus:outline-hidden dark:bg-gray-700 dark:text-white placeholder-gray-400 transition-colors"
+            />
+          </div>
+          {/* requirements */}
+          <div>
+            <label
+              htmlFor="requirements"
+              className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2"
+            >
+              requirements *
+            </label>
 
-          <TextEditor
-            required
-            value={formData.requirements}
-            onChange={(value) =>
-              setFormData({ ...formData, requirements: value })
-            }
-          />
-        </div>
-      </form>
+            <TextEditor
+              required
+              value={formData.requirements}
+              onChange={(value) =>
+                setFormData({ ...formData, requirements: value })
+              }
+            />
+          </div>
+        </form>
+      </div>
     </Alert>
   );
 };
