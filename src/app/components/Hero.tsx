c6f551e064342c84bac21e987fc1528dd9384011
Kudae Sithu <kudaesithu2@gmail.com>
2025-06-14 11:20:34 +0630

    feat: add SplitText component for animated text and integrate with Hero section; update package dependencies

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,17 +1,25 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
 import ShinyText from "@/components/ui/shiny-text";
+import SplitText from "@/components/ui/split-text";
 
 const Hero = () => {
   return (
     <section className="bg-white lg:grid lg:h-screen lg:place-content-center dark:bg-gray-900 pt-12 md:pt-18 lg:pt-0">
       <div className="mx-auto w-screen max-w-screen-xl px-4 py-16 sm:px-6 md:grid md:grid-cols-2 md:items-center md:gap-4 lg:px-8 lg:py-0">
         <div className="max-w-prose text-left">
-          <h1 className="text-4xl font-bold text-gray-900 sm:text-5xl dark:text-white">
-            Transform Your Business with
-            <strong className="text-primary"> Modern Technology </strong>
-            Solutions
-          </h1>
+          <SplitText
+            text="Empowering Your Business with Innovative Digital Solutions"
+            className="text-4xl font-bold text-gray-900 sm:text-5xl dark:text-white"
+            delay={100}
+            duration={0.3}
+            ease="power3.out"
+            splitType="chars"
+            from={{ opacity: 0, y: 40 }}
+            to={{ opacity: 1, y: 0 }}
+            threshold={0.1}
+            rootMargin="-100px"
+          />
 
           <ShinyText
             text="Stay ahead of the competition with cutting-edge digital solutions.
